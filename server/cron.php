9b4b8c6872fbfb346899121252594b3778fc568c
benlevine1 <benlevine1@gmail.com>
2019-05-09 13:36:06 -0700

    changed cron job

--- server/cron.php
+++ server/cron.php
@@ -8,7 +8,7 @@ require_once('mysqlcredentials.php');
 
 $student_id = intval( $POST['student_id']);
 
-$query = "DELETE FROM `sgt` WHERE `id` > 100";
+$query = "TRUNCATE TABLE 'sgt'";
 
 $result = mysqli_query($db, $query);
 
@@ -25,5 +25,24 @@ if( $result ){
 $json_output = json_encode($output);
 
 print($json_output);
+
+$query = "INSERT INTO `sgt` (`id`, `name`, `course`, `grade`) VALUES (NULL, 'Mary Shields', 'Javascript', '75'), (NULL, 'Federico Brown', 'Data Structures', '83'), (NULL, 'Frank Roye', 'Web Development 101', '94'), (NULL, 'Michelle Odom', 'Backend Architecture ', '87'), (NULL, 'Howard Allen', 'App Development', '97')";
+
+$result = mysqli_query($db, $query);
+
+if( $result ){
+    if(mysqli_affected_rows($db) ===1) {
+        $output['success'] = true;
+    } else {
+        $output['error'] = 'Could not delete student.';
+    }
+} else {
+    $output['error'] = mysqli_error($db);
+}
+
+$json_output = json_encode($output);
+
+print($json_output);
+
 
 ?>
